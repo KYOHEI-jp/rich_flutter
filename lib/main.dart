2f6ca444654d347cb72b51e0fbe4f55f3e3e587b
渡辺恭平 <kingdomhearts510@gmail.com>
2021-05-22 16:52:24 +0900

    image

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
-
 //main関数から始まるよ。
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.redAccent,
         appBar: AppBar(
-          title: Text("I'm great"),
+          title: Text("I'm superman"),
+          backgroundColor: Colors.yellow,
+        ),
+        body: Image(
+          image: NetworkImage("http://www.horiyamanoie.com/wp-content/uploads/sites/29/2020/02/mount_fuji.jpg"),
         ),
       ),
     ),
